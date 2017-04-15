fed8271441d25ff50c1b469c3ef8045fb2e54896
Krati Ahuja <kratiahuja@gmail.com>
2017-04-14 22:50:23 -0700

    Example of appending to fastboot manifest.

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 /* eslint-env node */
 'use strict';
+var path = require('path');
+var BroccoliMergeTrees = require('broccoli-merge-trees');
+var Concat = require('broccoli-concat');
+var Funnel = require('broccoli-funnel');
 
 module.exports = {
-  name: 'ember-fastboot-build-example'
+  name: 'ember-fastboot-build-example',
+
+  treeForPublic: function(tree) {
+    var fooTree = new Funnel('vendor', {
+      destDir: 'assets'
+    });
+
+    if (tree) {
+      return new BroccoliMergeTrees[tree, fooTree];
+    }
+
+    return tree;
+  },
+
+  appendFastBootManifest: function() {
+    return {
+      vendorFiles: ['assets/foo.js'],
+      appFiles: ['assets/bar.js']
+    }
+  }
 };
